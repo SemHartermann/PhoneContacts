0590a98d7b649b408419175272602c29ac908530
Sem Zhivotov <semzhivotov@gmail.com>
2023-07-06 00:37:25 +0300

    feat(contact): add validation and exception handling

---
+++ src/main/java/com/chiit/phonecontacts/exceptions/ContactNotFoundException.java
@@ -0,0 +1,8 @@
+package com.chiit.phonecontacts.exceptions;
+
+public class ContactNotFoundException extends Exception {
+
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/chiit/phonecontacts/validations/PhoneNumberPatternValidator.java
@@ -0,0 +1,26 @@
+package com.chiit.phonecontacts.validations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class PhoneNumberPatternValidator implements ConstraintValidator<PhoneNumberPattern, Set<String>> {
+    private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("(\\+380\\d{9})");
+
+    @Override
+    public boolean isValid(Set<String> phoneNumbers, ConstraintValidatorContext context) {
+
+        if (phoneNumbers.isEmpty()){
+            return false;
+        }
+
+        for (String phoneNumber: phoneNumbers) {
+            if(!PHONE_NUMBER_PATTERN.matcher(phoneNumber).matches()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
